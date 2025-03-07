92360510470f6706ae7622bd0a7ef2e64dd2e65e
1sumit1 <sumitnehra02@gmail.com>
2025-03-07 20:41:31 +0530

    Add script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='app'>
+      <div className='abc'>
         <Header />
         <MarketingApp />
       </div>
